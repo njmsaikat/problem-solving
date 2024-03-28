78e3b6841e6ebf8866952386b136ec55989aa963
Saikat Roy <njmsaikat@gmail.com>
2024-03-28 06:39:35 +0600

    update divisible_sum_pairs.py

--- hackerrank/divisible_sum_pairs.py
+++ hackerrank/divisible_sum_pairs.py
@@ -2,15 +2,11 @@ n=6
 k=3
 ar=[1, 3, 2, 6, 1, 2]
 def divisibleSumPairs(n, k, ar):
-    ar=sorted(ar)
-    count=0
-    for i in range(n
-    
-    
-    
-    ):
-        print(ar[i])
-        if (ar[i]<ar[i+1]) and ((ar[i+1]+ar[i])%k) == 0:
-            count += 1
-    print(count)
+    counter = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if(ar[i]+ar[j])%k ==0:
+                counter +=1
+    # return counter
+    print(counter)
 divisibleSumPairs(n, k, ar)
